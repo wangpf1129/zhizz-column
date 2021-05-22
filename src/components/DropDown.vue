a4de05a831820613b6d656ba0359eff66db44dfd
Wangpf <1204981117@qq.com>
2021-05-22 22:40:04 +0800

    完成下拉组件的基本结构和交互

---
+++ src/components/DropDown.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="userDropDown dropdown">
+    <a href="#" class="btn btn-outline-success my-2 px-4 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
+    <transition name="fade">
+      <ul class="userDropDownBox dropdown-menu" :style="{display:'block'}" v-if="isOpen">
+        <li class="dropdown-item">
+          <span> 新建文章 </span>
+        </li>
+        <li class="dropdown-item">
+          <span> 编辑资料 </span>
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+export default defineComponent({
+  name: 'DropDown',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const isOpen = ref(false);
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value;
+    };
+    return {
+      isOpen, toggleOpen
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.userDropDown {
+  outline: none;
+  cursor: pointer;
+  
+  .userDropDownBox {
+    margin-top: 15px;
+    border: none;
+    box-shadow: 0 5px 20px rgb(18 18 18 / 10%);
+    
+    :hover {
+      background-color: #f6f6f6;
+    }
+    
+    > li {
+      font-size: 14px;
+      padding: 13px 16px;
+      color: #8590a6;
+      outline: none;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      vertical-align: middle;
+    }
+  }
+  
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+}
+</style>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="ColumnPageHeader">
-    <nav class="header navbar bg-body justify-content-between px-4">
-      <a class="title navbar-brand" href="#"> 清栀专栏 </a>
-      <ul v-if="!user.isLogin" class="list-inline mb-0">
+    <nav class="header navbar bg-body justify-content-between px-4 py-0">
+      <a class="headerTitle navbar-brand" href="#"> 清栀专栏 </a>
+      <ul v-if="!user.isLogin" class="headerButton list-inline mb-0">
         <li class="list-inline-item">
           <a href="#" class="btn btn-outline-success  my-2 px-4 "> 登录 </a>
           <a href="#" class="btn btn-outline-success  my-2 px-4"> 注册 </a>
         </li>
       </ul>
-      <ul v-else class="list-inline mb-0">
+      <ul v-else class="headerButton list-inline mb-0">
         <li class="list-inline-item">
-          <a href="#" class="btn btn-outline-success my-2"> 你好，{{ user.name }} </a>
+          <drop-down :title="user.name"></drop-down>
         </li>
       </ul>
     </nav>
@@ -19,6 +19,7 @@
 
 <script lang="ts">
 import {defineComponent, PropType} from 'vue';
+import DropDown from '@/components/DropDown.vue';
 
 export interface UserProps {
   isLogin: boolean;
@@ -28,6 +29,7 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {DropDown},
   props: {
     user: {
       type: Object as PropType<UserProps>,
@@ -55,8 +57,13 @@ export default defineComponent({
       margin-left: 15px;
     }
     
-    .title {
+    .headerTitle {
       color: #40babf;
+      margin-left: 66px;
+    }
+    
+    .headerButton {
+      margin-right: 66px;
     }
   }
 }
