5ef24bd53e4f1f531ad71b166bba783ca765d9ff
Wangpf <1204981117@qq.com>
2021-05-22 21:51:09 +0800

    初始化项目，并完成 ColumnList 和 GlobalHeader 组件的基本结构与样式

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="columnList row">
+    <h3 class="ColumnHomeTitle">专栏 · 发现</h3>
+    <div class="col-3 mb-4 mt-2 " v-for="column in columnList" :key="column.id">
+      <div class="columnItem card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" :alt="column.title" class="rounded-circle w-25 my-3 border border-light">
+          <h5 class="columnTitle card-title mt-1">{{ column.title }}</h5>
+          <p class="columnText card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-success mt-2 mb-1"> 进入专栏 </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, computed, PropType} from 'vue';
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar?: string;
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {type: Array as PropType<ColumnProps[]>, required: true}
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/column.png');
+        }
+        return column;
+      });
+    });
+    return {columnList};
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.columnList {
+  margin: 108px auto 0;
+  
+  .ColumnHomeTitle {
+    font-size: 16px;
+    line-height: 22px;
+    font-weight: 600;
+    -webkit-box-align: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin: 0 3px 32px;
+    width: 100%;
+  }
+  .columnItem {
+    border: none;
+  }
+  .columnTitle {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  
+  .columnText {
+    font-size: 14px;
+    line-height: 21px;
+    min-height: 42px;
+    margin-top: 7px;
+    word-break: break-all;
+    display: -webkit-box;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+}
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="ColumnPageHeader">
+    <nav class="header navbar bg-body justify-content-between px-4">
+      <a class="title navbar-brand" href="#"> 清栀专栏 </a>
+      <ul v-if="!user.isLogin" class="list-inline mb-0">
+        <li class="list-inline-item">
+          <a href="#" class="btn btn-outline-success  my-2 px-4 "> 登录 </a>
+          <a href="#" class="btn btn-outline-success  my-2 px-4"> 注册 </a>
+        </li>
+      </ul>
+      <ul v-else class="list-inline mb-0">
+        <li class="list-inline-item">
+          <a href="#" class="btn btn-outline-success my-2"> 你好，{{ user.name }} </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+
+export interface UserProps {
+  isLogin: boolean;
+  id?: number;
+  name?: string;
+}
+
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.ColumnPageHeader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1001;
+  width: 100%;
+  height: 52px;
+  
+  .header {
+    box-sizing: border-box;
+    box-shadow: 0 1px 3px rgb(18 18 18 / 10%);
+    
+    .btn {
+      margin-left: 15px;
+    }
+    
+    .title {
+      color: #40babf;
+    }
+  }
+}
+</style>
