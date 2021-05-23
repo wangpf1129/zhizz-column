164291a1699ee13403363012603297db78aaa103
Wangpf <1204981117@qq.com>
2021-05-23 15:12:06 +0800

    点击外部区域隐藏下拉框

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -8,7 +8,7 @@ export const useClickOutside = (elementRef: Ref<null | HTMLElement>): Ref<boolea
   const handler = (event: MouseEvent) => {
     if (elementRef.value) {
       // 点击的区域如果不是 elementRef区域的内容，则设为 true， 否则设为 false
-      isClickOutside.value = !elementRef.value?.contains(event.target as HTMLElement);
+      isClickOutside.value = !elementRef.value.contains(event.target as HTMLElement);
     }
   };
   onMounted(() => {
