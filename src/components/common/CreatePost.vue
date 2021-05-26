37d68d3065240b2c0f59e059bf66a1eae2696d9c
Wangpf <1204981117@qq.com>
2021-05-26 23:07:46 +0800

    实现文件上传给服务器

--- src/components/common/CreatePost.vue
+++ src/components/common/CreatePost.vue
@@ -2,24 +2,27 @@
   <div class="CreatePostPage">
     <h4>新建文章</h4>
     <validate-form @form-submit="onFormSubmit">
+      <input type="file" name="file" @change="handleFileChange" />
       <div class="mb-3">
-        <validate-input input-title="文章标题"
-                        :rules="titleRules"
-                        v-model:input-value="titleValue"
-                        type="text"
-                        placeholder="请输入文章标题"
-                        class="login-input-item"
+        <validate-input
+          input-title="文章标题"
+          :rules="titleRules"
+          v-model:input-value="titleValue"
+          type="text"
+          placeholder="请输入文章标题"
+          class="login-input-item"
         ></validate-input>
       </div>
       <div class="mb-3">
-        <validate-input input-title="文章详情"
-                        tag="textarea"
-                        rows="10"
-                        :rules="contentRules"
-                        v-model:input-value="contentValue"
-                        type="text"
-                        placeholder="请输入文章内容"
-                        class="login-input-item"
+        <validate-input
+          input-title="文章详情"
+          tag="textarea"
+          rows="10"
+          :rules="contentRules"
+          v-model:input-value="contentValue"
+          type="text"
+          placeholder="请输入文章内容"
+          class="login-input-item"
         ></validate-input>
       </div>
       <template #submit>
@@ -32,55 +35,78 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
-import ValidateForm from '@/components/content/ValidateForm.vue';
-import ValidateInput from '@/components/content/ValidateInput.vue';
-import {RulesProp} from '@/common/inputRules';
-import store, {PostProps} from '@/store';
-import router from '@/router';
+import { defineComponent, ref } from 'vue'
+import ValidateForm from '@/components/content/ValidateForm.vue'
+import ValidateInput from '@/components/content/ValidateInput.vue'
+import { RulesProp } from '@/common/inputRules'
+import store, { PostProps } from '@/store'
+import router from '@/router'
+import axios from 'axios'
 
 export default defineComponent({
   name: 'CreatePost',
-  components: {ValidateInput, ValidateForm},
+  components: { ValidateInput, ValidateForm },
   setup() {
     const titleRules: RulesProp = [
-      {type: 'required', message: '文章标题不能为空'},
-    ];
+      { type: 'required', message: '文章标题不能为空' },
+    ]
     const contentRules: RulesProp = [
-      {type: 'required', message: '文章内容不能为空'},
-    ];
-    const titleValue = ref(''); //  Wangpf@163.com
-    const contentValue = ref(''); // w12345678
+      { type: 'required', message: '文章内容不能为空' },
+    ]
+    const titleValue = ref('') //  Wangpf@163.com
+    const contentValue = ref('') // w12345678
     const onFormSubmit = (result: boolean) => {
       if (result) {
-        const {column} = store.state.user;
+        const { column } = store.state.user
         if (column) {
           const newPost: PostProps = {
-            _id: new Date().getTime() + '',
             title: titleValue.value,
             content: contentValue.value,
             column: column,
-            createdAt: new Date().toLocaleString()
-          };
-          store.commit('createNewPost', newPost);
-          router.push({name: 'ColumnDetail', params: {id: column}});
+          }
+          store.commit('createNewPost', newPost)
+          router.push({ name: 'ColumnDetail', params: { id: column } })
         }
       }
-    };
-    return {titleRules, titleValue, contentRules, contentValue, onFormSubmit};
-  }
-});
+    }
+    const handleFileChange = (event: Event) => {
+      const target = event.target as HTMLInputElement
+      const files = target.files
+      if (files) {
+        const uploadedFile = files[0]
+        const formData = new FormData()
+        formData.append(uploadedFile.name, uploadedFile)
+        axios
+          .post('/upload', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+          .then((resp) => {
+            console.log(resp)
+          })
+      }
+    }
+    return {
+      titleRules,
+      titleValue,
+      contentRules,
+      contentValue,
+      onFormSubmit,
+      handleFileChange,
+    }
+  },
+})
 </script>
 
 <style lang="scss" scoped>
-
 .CreatePostPage {
   margin: 76px auto 0;
-  
+
   .login-btn-wrap {
     text-align: center;
     margin: 24px 0;
-    
+
     > span {
       padding: 8px 36px;
     }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,129 +1,137 @@
-import {Commit, createStore} from 'vuex';
-import axios from 'axios';
-
+import { Commit, createStore } from 'vuex'
+import axios from 'axios'
 
 export interface UserProps {
-  isLogin: boolean;
-  nickName?: string;
-  _id?: string;
-  column?: string;
-  email?: string;
+  isLogin: boolean
+  nickName?: string
+  _id?: string
+  column?: string
+  email?: string
 }
 
 interface GlobalErrorProps {
-  status: boolean;
-  message?: string;
+  status: boolean
+  message?: string
 }
 
 export interface GlobalDataProps {
-  error: GlobalErrorProps;
-  token: string;
-  isLoading: boolean;
-  columns: ColumnProps[];
-  posts: PostProps[];
+  error: GlobalErrorProps
+  token: string
+  isLoading: boolean
+  columns: ColumnProps[]
+  posts: PostProps[]
   user: UserProps
 }
 
 interface ImageProps {
-  _id?: string;
-  url?: string;
+  _id?: string
+  url?: string
   createdAt?: string
 }
 
 export interface ColumnProps {
-  _id: string;
-  title: string;
-  avatar?: ImageProps;
+  _id: string
+  title: string
+  avatar?: ImageProps
   description: string
 }
 
 export interface PostProps {
-  _id: string;
-  title: string;
-  excerpt?: string;
-  content?: string;
-  image?: ImageProps;
-  createdAt: string;
+  _id?: string
+  title: string
+  excerpt?: string
+  content?: string
+  image?: ImageProps
+  createdAt?: string
   column: string
 }
 
-const getAndCommit = async (url: string, mutationsName: string, commit: Commit) => {
-  const {data} = await axios.get(url);
-  commit(mutationsName, data);
-};
-const postAndCommit = async (url: string, mutationsName: string, commit: Commit, payload: any) => {
-  const {data} = await axios.post(url, payload);
-  commit(mutationsName, data);
-  return data;
-};
+const getAndCommit = async (
+  url: string,
+  mutationsName: string,
+  commit: Commit
+) => {
+  const { data } = await axios.get(url)
+  commit(mutationsName, data)
+}
+const postAndCommit = async (
+  url: string,
+  mutationsName: string,
+  commit: Commit,
+  payload: any
+) => {
+  const { data } = await axios.post(url, payload)
+  commit(mutationsName, data)
+  return data
+}
 const store = createStore<GlobalDataProps>({
   state: {
-    error: {status: false},
+    error: { status: false },
     token: localStorage.getItem('columnToken') || '',
     isLoading: false,
     columns: [],
     posts: [],
-    user: {isLogin: false}
+    user: { isLogin: false },
   },
   getters: {
     getColumnById: (state) => (id: string) => {
-      return state.columns.find(column => column._id === id);
+      return state.columns.find((column) => column._id === id)
     },
     getPostsByCid: (state) => (Cid: string) => {
-      return state.posts.filter(post => post.column === Cid);
-    }
+      return state.posts.filter((post) => post.column === Cid)
+    },
   },
   mutations: {
     createNewPost(state, newPost) {
-      state.posts.push(newPost);
+      state.posts.push(newPost)
     },
     fetchColumns(state, rawData) {
-      state.columns = rawData.data.list;
+      state.columns = rawData.data.list
     },
     fetchColumn(state, rawData) {
-      state.columns = [rawData.data];
+      state.columns = [rawData.data]
     },
     fetchPosts(state, rawData) {
-      state.posts = rawData.data.list;
+      state.posts = rawData.data.list
     },
     setLoading(state, status) {
-      state.isLoading = status;
+      state.isLoading = status
     },
     setError(state, err: GlobalErrorProps) {
-      state.error = err;
+      state.error = err
     },
     login(state, rawData) {
-      const {token} = rawData.data;
-      state.token = token;
-      localStorage.setItem('columnToken', token);
-      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { token } = rawData.data
+      state.token = token
+      localStorage.setItem('columnToken', token)
+      axios.defaults.headers.Authorization = `Bearer ${token}`
     },
     fetchCurrentUser(state, rawData) {
-      state.user = {isLogin: true, ...rawData.data};
-    }
+      state.user = { isLogin: true, ...rawData.data }
+    },
   },
   actions: {
-    fetchColumns({commit}) {
-      getAndCommit('/columns', 'fetchColumns', commit);
+    fetchColumns({ commit }) {
+      getAndCommit('/columns', 'fetchColumns', commit)
     },
-    fetchColumn({commit}, Cid) {
-      getAndCommit(`/columns/${Cid}`, 'fetchColumn', commit);
+    fetchColumn({ commit }, Cid) {
+      getAndCommit(`/columns/${Cid}`, 'fetchColumn', commit)
     },
-    fetchPosts({commit}, Cid) {
-      getAndCommit(`/columns/${Cid}/posts`, 'fetchPosts', commit);
+    fetchPosts({ commit }, Cid) {
+      getAndCommit(`/columns/${Cid}/posts`, 'fetchPosts', commit)
     },
-    fetchCurrentUser({commit}) {
-      getAndCommit(`/user/current`, 'fetchCurrentUser', commit);
+    fetchCurrentUser({ commit }) {
+      getAndCommit(`/user/current`, 'fetchCurrentUser', commit)
     },
-    login({commit}, payload) {
-      return postAndCommit('/user/login', 'login', commit, payload);
+    login({ commit }, payload) {
+      return postAndCommit('/user/login', 'login', commit, payload)
     },
-    loginAndFetchCurrentUser({dispatch}, loginData) {
+    loginAndFetchCurrentUser({ dispatch }, loginData) {
       return dispatch('login', loginData).then(() => {
-        return dispatch('fetchCurrentUser');
-      });
-    }
-  }
-});
+        return dispatch('fetchCurrentUser')
+      })
+    },
+  },
+})
 
-export default store;
+export default store
